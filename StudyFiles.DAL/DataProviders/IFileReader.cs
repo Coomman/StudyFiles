ecc8f697dcd6f37bd43d509fd02b1b693e5aee31
Maxim Rauba <Maxim.Rauba@veeamacademy.ru>
2020-07-17 04:08:41 +0000

    CleanUp
    git-tfs-id: [https://dev.azure.com/veeamacademy/]$/Maxim.Rauba/StudyFiles;C1105

--- StudyFiles.DAL/DataProviders/IFileReader.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudyFiles.DAL.DataProviders
-{
-    public interface IFileReader
-    {
-        public bool FileSearch(string filePath, string searchQuery);
-        public string SaveFile(string filePath, byte[] data);
-    }
-}
